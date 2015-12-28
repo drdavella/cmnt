734240ff700b1df3e1c0b88fb87fb1dec0c1797b
Dan D'Avella <drdavella@gmail.com>
2015-12-28 18:56:15 -0500

    Implemented capability to add comments along with simple test executable

--- src/cpp/comment.cpp
+++ src/cpp/comment.cpp
@@ -1,8 +1,26 @@
+#include <cstring>
+#include <error.h>
+#include <errno.h>
 #include <comment.hpp>
+
+#define CMNT_LEN_FIELD  "user.cmnt-len"
+#define CMNT_MSG_FIELD  "user.cmnt-msg"
 
 
 int add_comment(const std::string path, const std::string comment)
 {
+    size_t len = comment.size();
+    // currently creates new, or replaces if already exists
+    if (setxattr(path.c_str(),CMNT_LEN_FIELD,&len,sizeof(len),0) != 0)
+    {
+        fprintf(stderr,"error adding length field: %s\n",strerror(errno));
+        return errno;
+    }
+    if (setxattr(path.c_str(),CMNT_MSG_FIELD,comment.c_str(),len,0) != 0)
+    {
+        fprintf(stderr,"error adding comment field: %s\n",strerror(errno));
+        return errno;
+    }
     return 0;
 }
 
---
+++ src/cpp/test/add_comment.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <boost/program_options.hpp>
+#include <comment.hpp>
+
+
+int main(int argc, char ** argv)
+{
+    add_comment(argv[1],"testing");
+
+
+}
